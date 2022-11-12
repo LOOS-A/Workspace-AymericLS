f1080ee8b06234ec2a0de3d333803ae05237b535
LOOSAY <gable67g2@gmail.com>
2022-11-12 17:18:31 +0100

    Deleting useless files and repos

--- LocalLibrary/express-locallibrary-tutorial/routes/dataObjects.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Respond with a list of objects !');
-});
-
-module.exports = router;
